973283e16842060eafa838296714206525e9eb02
Matic Zarnec <matic.blender@gmail.com>
2023-12-03 10:42:10 +0100

    chore: cleanup

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,24 @@
-import AppRouter from "@/pages/app-router.tsx";
+import useInitialize from "@/hooks/use-initialize.ts";
+import useAccountStore from "@/store/account-store.ts";
+import LoadingPage from "@/pages/loading-page.tsx";
+import AuthRouter from "@/pages/auth-router/auth-router.tsx";
+import DashboardRouter from "@/pages/dashboard-router/dashboard-router.tsx";
 
 function App() {
-  return <AppRouter />;
+  const { loading } = useInitialize();
+  const {
+    computed: { isLoggedIn },
+  } = useAccountStore();
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
+  if (!isLoggedIn) {
+    return <AuthRouter />;
+  }
+
+  return <DashboardRouter />;
 }
 
 export default App;
