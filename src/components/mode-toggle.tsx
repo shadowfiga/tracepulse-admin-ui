204c5380d1089af67354e129fadace549838a027
Matic Zarnec <matic.blender@gmail.com>
2023-12-11 15:10:45 +0100

    feat: add theme toggle

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -15,11 +15,7 @@ const ModeToggle = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          className="dark:bg-slate-500 bg-white"
-          size="icon"
-        >
+        <Button variant="outline" className="bg-background" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
--- src/pages/auth-router/auth-router.tsx
+++ src/pages/auth-router/auth-router.tsx
@@ -9,10 +9,11 @@ import { AppRoutes } from "@/constants/app-routes.ts";
 import LoginPage from "@/pages/auth-router/login-page.tsx";
 import ForgotPasswordPage from "@/pages/auth-router/forgot-password-page.tsx";
 import ResetPasswordPage from "@/pages/auth-router/reset-password-page.tsx";
+import ModeToggle from "@/components/mode-toggle.tsx";
 
 const AuthRouter: FC = () => {
   return (
-    <main className="bg-background w-full h-screen flex flex-col items-center justify-center bg-[url(/circuit-board.svg)] dark:bg-[url(/circuit-board-dark.svg)]">
+    <main className="relative bg-background w-full h-screen flex flex-col items-center justify-center bg-[url(/circuit-board.svg)] dark:bg-[url(/circuit-board-dark.svg)]">
       <Router>
         <Routes>
           <Route path={AppRoutes.login} element={<LoginPage />} />
@@ -27,6 +28,9 @@ const AuthRouter: FC = () => {
           <Route path="*" element={<Navigate to={AppRoutes.login} />} />
         </Routes>
       </Router>
+      <div className="absolute top-[20px] right-[20px]">
+        <ModeToggle />
+      </div>
     </main>
   );
 };
